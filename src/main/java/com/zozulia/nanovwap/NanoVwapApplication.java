e37d3f8d55c67ce1d007de06f1b75dcb17ae71a3
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2026-05-05 04:06:36 +0200

    Initial commit

---
+++ src/main/java/com/zozulia/nanovwap/NanoVwapApplication.java
@@ -0,0 +1,13 @@
+package com.zozulia.nanovwap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanoVwapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NanoVwapApplication.class, args);
+    }
+
+}
